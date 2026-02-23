958df117eab174a7309eb386e6a9ddc5bce17271
Kateryna Koliada <6parkseolhwa6@gmail.com>
2026-02-23 15:39:05 +0100

    Add hover/active styles for buttons. Finish setting up media queries. Change auth check

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,17 @@
 import PopularStories from "../../components/PopularStories/PopularStories";
-import TravellersStoriesItem from "../../components/TravellersStoriesItem/TravellersStoriesItem";
 
+import "@/src/app/globals.css";
 export default function HomePage() {
   return (
     <main>
-      <h2>Hero</h2>
-      <h2>About</h2>
-      <h2>PopularStoriesSection</h2>
-      <h2>OurTravellers</h2>
-      <h3>Join</h3>
-      
+      <div className="container">
+        <h2>Hero</h2>
+        <h2>About</h2>
+        <h2>PopularStoriesSection</h2>
+        <PopularStories />
+        <h2>OurTravellers</h2>
+        <h3>Join</h3>
+      </div>
     </main>
   );
 }
