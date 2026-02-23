44a7abc8c1531a44da25d06122a5dd884372b837
87233 <8723323@gmail.com>
2026-02-23 23:07:11 +0200

    join

--- src/components/Join/Join.tsx
+++ src/components/Join/Join.tsx
@@ -1,17 +1,31 @@
+"use client";
 import css from "./Join.module.css";
-
+import Link from "next/link";
+import { useAuthStore } from "@/src/lib/store/authStore";
 const Join = () => {
+  const authenticated = useAuthStore((state) => state.isAuthenticated);
   return (
-    <main>
-      <div className={css.container}>
-        <h1 className={css.title}>Приєднуйтесь до нашої спільноти</h1>
-        <p className={css.description}>
-          Долучайтеся до мандрівників, які діляться своїми історіями та
-          надихають на нові пригоди.
-        </p>
-        <button>Зареєструватися</button>
+    <section className={css.join}>
+      <div className={css.join_background}>
+        <div className={css.join_content}>
+          <h2 className={css.join_title}>Приєднуйтесь до нашої спільноти</h2>
+          <p className={css.join_description}>
+            Долучайтеся до мандрівників, які діляться своїми історіями та
+            надихають на нові пригоди.
+          </p>
+
+          {authenticated ? (
+            <Link className={css.join_button} href="/auth/profile">
+              Збережені
+            </Link>
+          ) : (
+            <Link className={css.join_button} href="/auth/register">
+              Зареєструватися
+            </Link>
+          )}
+        </div>
       </div>
-    </main>
+    </section>
   );
 };
 
