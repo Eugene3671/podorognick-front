f566a19643fcfb165fa4e5eab73034c146f06c8a
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-23 17:20:58 +0200

    add hero section in components and add color in global css for my button and text

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,17 @@
+import Hero from "@/src/components/Hero/Hero";
 import PopularStories from "../../components/PopularStories/PopularStories";
 import TravellersStoriesItem from "../../components/TravellersStoriesItem/TravellersStoriesItem";
 
 export default function HomePage() {
   return (
     <main>
-      <h2>Hero</h2>
-      <h2>About</h2>
-      <h2>PopularStoriesSection</h2>
-      <h2>OurTravellers</h2>
-      <h3>Join</h3>
-      
+      <div className="container">
+        <Hero />
+        <h2>About</h2>
+        <h2>PopularStoriesSection</h2>
+        <h2>OurTravellers</h2>
+        <h3>Join</h3>
+      </div>
     </main>
   );
 }
