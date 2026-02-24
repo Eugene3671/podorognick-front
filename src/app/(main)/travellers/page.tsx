3e2cdacf3a89ccdb7905deec8f0f21d6e2fdf6cd
Maksym Zhelezniak <zzzeleznyakmaxim@gmail.com>
2026-02-24 22:01:30 +0200

    add travelers

--- src/app/(main)/travellers/page.tsx
+++ src/app/(main)/travellers/page.tsx
@@ -1,7 +1,45 @@
-import React from "react"
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { getUsers } from "@/src/lib/services/users.service"; // или точный путь к getUsers
+import { User } from "@/src/types/user"; // если у тебя есть тип User
 
 const TravellersPage = () => {
-  return <div>Travellers main page placeholder</div>
-}
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-export default TravellersPage
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data.users); 
+      } catch (err) {
+        console.error(err);
+        setError("Ошибка при загрузке пользователей");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>Загрузка...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      <h1>Список пользователей</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name} — {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TravellersPage;
