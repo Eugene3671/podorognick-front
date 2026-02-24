38c2faf8160339ec988564491eb30785854362df
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-24 22:11:20 +0200

    delete fix size from container in section hero and about

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,15 +6,13 @@ import "@/src/app/globals.css";
 export default function HomePage() {
   return (
     <main>
-      <div className="container">
-        <Hero />
-        <About />
-        <h2>PopularStoriesSection</h2>
-        <h2>PopularStoriesSection</h2>
-        <PopularStories />
-        <h2>OurTravellers</h2>
-        <h3>Join</h3>
-      </div>
+      <Hero />
+      <About />
+      <h2>PopularStoriesSection</h2>
+      <h2>PopularStoriesSection</h2>
+      <PopularStories />
+      <h2>OurTravellers</h2>
+      <h3>Join</h3>
     </main>
   );
 }
