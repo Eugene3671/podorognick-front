940af90651aebda041898f0a233c6d4a7c6b8476
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-24 14:47:24 +0200

    change css from hero and add picture in html

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,36 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <div className={css.container}>
+      <picture className={css.picture}>
+        {/* desktop */}
+        <source
+          media="(min-width: 1440px)"
+          srcSet="
+      /img/hero-baner/bg-pc-compressed.jpg 1x,
+      /img/hero-baner/bg-pc@2x-compressed.jpg 2x
+    "
+        />
+
+        {/* tablet */}
+        <source
+          media="(min-width: 768px)"
+          srcSet="
+      /img/hero-baner/bg-tab-compressed.jpg 1x,
+      /img/hero-baner/bg-tab@2x-compressed.jpg 2x
+    "
+        />
+
+        {/* mobile (fallback) */}
+        <img
+          src="/img/hero-baner/bg-mob-compressed.jpg"
+          srcSet="
+      /img/hero-baner/bg-mob-compressed.jpg 1x,
+      /img/hero-baner/bg-mob@2x-compressed.jpg 2x
+    "
+          alt="Відкрита книга, плед і чашка кави"
+          className={css.image}
+        />
+      </picture>
       <h1 className={css.title}>Відкрийте світ подорожей з нами!</h1>
       <p className={css.description}>
         Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
