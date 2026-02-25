3644709e8b1565f5605924ee2446f4111935f44e
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-25 19:42:42 +0200

    add next-server in project

---
+++ src/app/api/api.ts
@@ -0,0 +1,10 @@
+// app/api/api.ts
+
+import axios, { AxiosError } from "axios";
+
+export type ApiError = AxiosError<{ error: string }>;
+
+export const api = axios.create({
+  baseURL: "https://podorognick-back.onrender.com/api",
+  withCredentials: true,
+});
