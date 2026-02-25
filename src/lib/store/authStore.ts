d5358e53fa17393617913c5077ffee604d5899dd
a-nabokova <itcw2s@gmail.com>
2026-02-25 16:23:59 +0200

    toater added in login /reg forms
    toater added in login /reg forms

--- src/lib/store/authStore.ts
+++ src/lib/store/authStore.ts
@@ -3,7 +3,6 @@ import type { User } from "../../types/user";
 
 type AuthStore = {
   isAuthenticated: boolean;
-  isLoading: boolean;
   user: User | null;
   setUser: (user: User, token?: string) => void;
   clearIsAuthenticated: () => void;
@@ -11,17 +10,16 @@ type AuthStore = {
 
 export const useAuthStore = create<AuthStore>()((set) => ({
   isAuthenticated: false,
-  isLoading: true,
   user: null,
 
   setUser: (user: User, token?: string) => {
     if (token) {
       localStorage.setItem("accessToken", token);
     }
-    set(() => ({ user, isAuthenticated: true, isLoading: false }));
+    set(() => ({ user, isAuthenticated: true }));
   },
   clearIsAuthenticated: () => {
     localStorage.removeItem("accessToken");
-    set(() => ({ user: null, isAuthenticated: false, isLoading: false }));
+    set(() => ({ user: null, isAuthenticated: false  }));
   },
 }));
