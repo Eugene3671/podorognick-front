ebdf44492f6f589a4b79dae174d49cc06066490e
Eugene_Korzh <korzh367@gmail.com>
2026-02-26 23:39:37 +0100

    change authStore

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { api } from "../../api";
+
+export async function GET() {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  if (!accessToken) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const res = await api.get("/auth/me", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    return NextResponse.json(res.data);
+  } catch (error) {
+    return NextResponse.json({ error: "Faild to fetch user" }, { status: 401 });
+  }
+}
