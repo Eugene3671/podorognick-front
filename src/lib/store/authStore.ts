a69b5a73fc83173a9b32ed6e36eb22264c6a816d
a-nabokova <itcw2s@gmail.com>
2026-02-27 17:45:16 +0200

    auth provider  and store update
    auth provider  and store update , styles for loader added

--- src/lib/store/authStore.ts
+++ src/lib/store/authStore.ts
@@ -1,4 +1,30 @@
+// import { create } from "zustand";
+// import type { User } from "../../types/user";
+
+// type AuthStore = {
+//   isAuthenticated: boolean;
+//   isLoading: boolean;
+//   user: User | null;
+//   setUser: (user: User) => void;
+//   clearIsAuthenticated: () => void;
+// };
+
+// export const useAuthStore = create<AuthStore>()((set) => ({
+//   isAuthenticated: false,
+//   isLoading: false,
+//   user: null,
+
+//   setUser: (user: User) => {
+//     set(() => ({ user, isAuthenticated: true }));
+//   },
+//   clearIsAuthenticated: () => {
+//     set(() => ({ user: null, isAuthenticated: false }));
+//   },
+// }));
+
+
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { User } from "../../types/user";
 
 type AuthStore = {
@@ -9,15 +35,23 @@ type AuthStore = {
   clearIsAuthenticated: () => void;
 };
 
-export const useAuthStore = create<AuthStore>()((set) => ({
-  isAuthenticated: false,
-  isLoading: false,
-  user: null,
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      isLoading: false,
+      user: null,
 
-  setUser: (user: User) => {
-    set(() => ({ user, isAuthenticated: true }));
-  },
-  clearIsAuthenticated: () => {
-    set(() => ({ user: null, isAuthenticated: false }));
-  },
-}));
+      setUser: (user: User) =>
+        set(() => ({ user, isAuthenticated: true })),
+
+      clearIsAuthenticated: () =>
+        set(() => ({ user: null, isAuthenticated: false })),
+    }),
+    {
+      name: "auth",  
+      partialize: (state) => ({ user: state.user, isAuthenticated: state.isAuthenticated }), 
+       
+    }
+  )
+);
