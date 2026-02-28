ae0033505da37910d190aac41f728a553086c9ca
87233 <8723323@gmail.com>
2026-02-28 22:28:52 +0200

    TravellerPage fix TravellerInfo

--- src/app/(main)/travellers/[travellerId]/page.tsx
+++ src/app/(main)/travellers/[travellerId]/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import TravellerInfo from "@/src/components/TravellerInfo/TravellerInfo";
+import TravellersStories from "@/src/components/TravellersStories/TravellersStories";
+import css from "./TravellerPage.module.css";
 
-const TravellerPage = () => {
-  return <div>Traveller page placeholder</div>;
+type Props = {
+  params: Promise<{ travellerId: string }>;
+};
+const TravellerPage = async ({ params }: Props) => {
+  const { travellerId } = await params;
+  return (
+    <div className="container">
+      <TravellerInfo travellerId={travellerId} />
+      <h1 className={css.title}>Історії Мандрівника</h1>
+      <TravellersStories
+        sort="new"
+        pageType="stories"
+        buttonType="loadMore"
+        mode="travellerStories"
+        ownerId={travellerId}
+      />
+    </div>
+  );
 };
 
 export default TravellerPage;
