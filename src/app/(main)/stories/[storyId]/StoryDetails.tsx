4bae6516bbdfc2cc835695168a94eaf25456e678
Eugene_Korzh <korzh367@gmail.com>
2026-03-01 23:17:05 +0100

    add to save

--- src/app/(main)/stories/[storyId]/StoryDetails.tsx
+++ src/app/(main)/stories/[storyId]/StoryDetails.tsx
@@ -1,11 +1,72 @@
+"use client";
 import { Story } from "@/src/types/story";
 import Image from "next/image";
 import css from "./StoryDetails.module.css";
 import { formatDate } from "@/src/utils/formatDate";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/src/lib/store/authStore";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import {
+  addToSavedStories,
+  removeFromSavedStories,
+} from "@/src/lib/api/storiesApi";
 interface StoryDetailsProps {
   story: Story;
 }
 export const StoryDetails = ({ story }: StoryDetailsProps) => {
+  const router = useRouter();
+  const { isAuthenticated, user } = useAuthStore();
+  const [favoriteCount, setFavoriteCount] = useState<number>(
+    story.favoriteCount,
+  );
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
+  const saveMutation = useMutation({
+    mutationFn: () => addToSavedStories(story._id),
+
+    onMutate: () => {
+      setIsSaved(true);
+      setFavoriteCount((favCount) => favCount + 1);
+    },
+
+    onError: (error) => {
+      setIsSaved(false);
+      setFavoriteCount((favCount) => favCount - 1);
+      toast.error(`Сталася помилка: ${error.message}`);
+    },
+    onSuccess: () => {
+      toast.success("Історію було збережено!");
+    },
+  });
+
+  const unsaveMutation = useMutation({
+    mutationFn: () => removeFromSavedStories(story._id),
+    onMutate: () => {
+      setIsSaved(false);
+      setFavoriteCount((favCount) => favCount - 1);
+    },
+    onError: (error) => {
+      setIsSaved(true);
+      setFavoriteCount((favCount) => favCount + 1);
+      toast.error(`Сталася помилка: ${error.message}`);
+    },
+    onSuccess: () => {
+      toast.success("Історію було видалено зі збережених.");
+    },
+  });
+
+  const handleSave = () => {
+    if (!isAuthenticated) {
+      router.push("/auth/login");
+      return;
+    }
+    saveMutation.mutate();
+  };
+
+  const handleUnsave = () => {
+    unsaveMutation.mutate();
+  };
   if (typeof story.category === "string" || typeof story.ownerId === "string")
     return <div>Помилка під час завантаженя</div>;
   return (
