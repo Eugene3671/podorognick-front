bd7549be8c658dc51e0529fa395e21d5a21ce2eb
Eugene_Korzh <korzh367@gmail.com>
2026-03-01 19:07:55 +0100

    story css details

--- src/app/(main)/stories/[storyId]/StoryDetails.tsx
+++ src/app/(main)/stories/[storyId]/StoryDetails.tsx
@@ -9,32 +9,31 @@ export const StoryDetails = ({ story }: StoryDetailsProps) => {
     return <div>Помилка під час завантаженя</div>;
   return (
     <>
-      <div className={css.storyDetails}>
-        <div>
-          <p>
-            <strong>Автор статті:{story.ownerId.name}</strong>
-          </p>
-          <p>
-            <strong>Опубліковано:{story.date}</strong>
-          </p>
-        </div>
-        <p>{story.category.name}</p>
+      <div className={css.storyInfo}>
+        <p className={css.storyInfoTitle}>
+          <strong>Автор статті </strong>
+          {story.ownerId.name}
+        </p>
+        <p className={css.storyInfoTitle}>
+          <strong>Опубліковано </strong>
+          {story.date}
+        </p>
+        <p className={css.storyInfoCategory}>{story.category.name}</p>
       </div>
-      <div>
-        <Image
-          src={story.img}
-          alt="story"
-          width={335}
-          height={223}
-          className="w-full h-auto rounded-lg"
-        />
+      <div className={css.storyImageWrapper}>
+        <Image src={story.img} alt="story" fill className={css.storyImage} />
       </div>
-      <div>
-        <p>{story.article}</p>
-        <div>
-          <p>Збережіть собі історію</p>
-          <p>Вона буде доступна у вашому профілі у розділі збережене</p>
-          <button type="button"> Зберегти aбо видали</button>
+      <div className={css.storyArticleWrapper}>
+        <p className={css.storyArticle}>{story.article}</p>
+        <div className={css.addToSaveWrapper}>
+          <p className={css.addToSaveTitle}>Збережіть собі історію</p>
+          <p className={css.addToSaveDescr}>
+            Вона буде доступна у вашому профілі у розділі збережене
+          </p>
+          <button className={`buttonBlue ${css.addToSaveBtn}`} type="button">
+            {" "}
+            Зберегти aбо видали
+          </button>
         </div>
       </div>
     </>
